198ea0d5bd461f7e7a552b7ba616b9f4b00e1527
李辉 <lihui.zl@163.com>
2018-05-24 12:38:01 +0800

    添加截屏功能

--- ByHouseMoney/LHMainTableViewController.m
+++ ByHouseMoney/LHMainTableViewController.m
@@ -72,6 +72,37 @@
     [self.navigationController pushViewController:detailViewController animated:YES];
 }
 
+#pragma mark - actions
+
+- (IBAction)saveScreen:(id)sender {
+    UIImage* image = nil;
+    // 下面方法，第一个参数表示区域大小。第二个参数表示是否是非透明的。如果需要显示半透明效果，需要传NO，否则传YES。第三个参数就是屏幕密度了，调整清晰度。
+    UIGraphicsBeginImageContextWithOptions(self.tableView.contentSize, YES, [UIScreen mainScreen].scale);
+    CGPoint savedContentOffset = self.tableView.contentOffset;
+    CGRect savedFrame = self.tableView.frame;
+    self.tableView.contentOffset = CGPointZero;
+    self.tableView.frame = CGRectMake(0, 0, self.tableView.contentSize.width, self.tableView.contentSize.height);
+    [self.tableView.layer renderInContext: UIGraphicsGetCurrentContext()];
+    image = UIGraphicsGetImageFromCurrentImageContext();
+    self.tableView.contentOffset = savedContentOffset;
+    self.tableView.frame = savedFrame;
+    
+    UIGraphicsEndImageContext();
+    
+    if (image != nil) {
+        //保存图片到相册
+        UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL);
+    }
+}
+
+- (void)image: (UIImage *) image didFinishSavingWithError: (NSError *) error contextInfo: (void *) contextInfo {
+    NSString *msg = error == NULL ? @"保存图片成功，可到相册查看" : @"保存图片失败";
+    UIAlertController *controller = [UIAlertController alertControllerWithTitle:nil message:msg preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil];
+    [controller addAction:action];
+    [self presentViewController:controller animated:YES completion:nil];
+}
+
 #pragma mark - LHDetailViewControllerDelegate
 - (void)detailViewController:(LHDetailViewController *)viewController finishBuildAccount:(LHAccount *)account {
     if (!account) {
